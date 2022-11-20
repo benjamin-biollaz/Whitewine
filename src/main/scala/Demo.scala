8cf70440eff0be4cbb84d51e89873c7c1546bfb7
benjamin <benjamin.biollaz@students.hevs.ch>
2022-11-20 20:46:54 +0100

    initial read method

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,3 +1,13 @@
 package demo
 
-@main def hello() = println("Hello world!")
+@main def hello() = {
+    println("Hello world!");
+    println("Month, Income, Expenses, Profit")
+    val bufferedSource = io.Source.fromFile("11-WhiteWine.csv")
+    for (line <- bufferedSource.getLines) {
+        val cols = line.split(",").map(_.trim)
+        // do whatever you want with the columns here
+        println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}")
+    }
+    bufferedSource.close
+}
