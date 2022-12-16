0fda6fa847cbf95ecd144fd9b9674be4fa25bda1
benjamin <benjamin.biollaz@students.hevs.ch>
2022-12-16 16:59:32 +0100

    change name, price, and rate

--- src/main/scala/classes/Wine.scala
+++ src/main/scala/classes/Wine.scala
@@ -1,5 +1,12 @@
 package classes
 
-class Wine (val name:String = "Porto", val year:Int = 2000, val price:Double = 29.90, val country: Country, val region: Region, val winery: Winery, val rating:Rating){
-    override def toString="Name:" + name + " Year:" + year + " Price:" + price + " " + country + " " + region + " " + winery+ " " + rating
+case class Wine(name: String = "Porto", year: Int = 2000, price: Double = 29.90, country: Country, region: Region, winery: Winery, rating: Rating) {
+  override def toString: String = "Name:" + name + " Year:" + year + " Price:" + price + " " + country + " " + region + " " + winery + " " + rating
+
+  //rate a wine calculates the rating average according to
+  //the numbers of ratings
+  val rateWine = (newRate: Double) => {
+    (this.rating.rating * this.rating.nb_rating + newRate) / (this.rating.nb_rating + 1)
+  }
+
 }
--- src/main/scala/display/DataManipulation.scala
+++ src/main/scala/display/DataManipulation.scala
@@ -1,11 +1,12 @@
 package display
 
 import dal.WineDB
+import classes.Rating
 
 import java.util.Scanner
 
 class DataManipulation {
-  val wineDB = new WineDB();
+  val wineDB = new WineDB()
 
     def getManipulateChoice = () => {
       println("What do you want to do ? \n" +
@@ -16,51 +17,64 @@ class DataManipulation {
         "5. Rate a wine?")
 
 
-      val scanChoice = new Scanner(System.in);
-      val inputChoice = scanChoice.nextLine();
+      val scanChoice = new Scanner(System.in)
+      val inputChoice = scanChoice.nextLine()
       inputChoice match {
-        case "1" => getInputAddWine();
-        case "2" => getInputRemoveWine();
-        case "3" => getInputChangeWineName();
-        case "4" => getInputChangeWinePrice();
-        case "5" => getInputRateWine();
+        case "1" => getInputAddWine()
+        case "2" => getInputRemoveWine()
+        case "3" => getInputChangeWineName()
+        case "4" => getInputChangeWinePrice()
+        case "5" => getInputRateWine()
       }
     }
 
   def getInput = (message: String) => {
     println(message)
-    val scan = new Scanner(System.in);
-    scan.nextLine();
+    val scan = new Scanner(System.in)
+    scan.nextLine()
   }
 
   def getInputAddWine = () => {
-    val inputAddWine = getInput("Which wine do you want to add?");
+    val inputAddWine = getInput("Which wine do you want to add?")
     // do something....
   }
 
   def getInputRemoveWine = () => {
-    val inputRemoveWine = getInput("Which wine do you want to remove?");
-    val wine = wineDB.getWineByName(inputRemoveWine);
+    val inputRemoveWine = getInput("Which wine do you want to remove?")
+    val wine = wineDB.getWineByName(inputRemoveWine)
     // do something....
   }
 
 
   def getInputChangeWineName = () => {
-    val inputChangeWineName = getInput("For which wine do you want to change its name?");
-    val wine = wineDB.getWineByName(inputChangeWineName);
-    // do something....
+    val inputChangeWineName = getInput("For which wine do you want to change its name?")
+    val wine = wineDB.getWineByName(inputChangeWineName)
+    println(wine)
+    val newName = getInput("How should we call it?")
+    //use of case class copy property to avoid concurrency problem
+    //in our case it is useless as no concurrent access is possible
+    val newWine = wine.copy(name = newName)
+    println(newWine)
   }
 
   def getInputChangeWinePrice = () => {
-    val inputChangeWinePrice = getInput("For which wine do you want to change the price?");
-    val wine = wineDB.getWineByName(inputChangeWinePrice);
-    // do something....
+    val inputChangeWinePrice = getInput("For which wine do you want to change the price?")
+    val wine = wineDB.getWineByName(inputChangeWinePrice)
+    val newPrice = getInput("How should we call it?")
+    //use of case class copy property to avoid concurrency problem
+    //in our case it is useless as no concurrent access is possible
+    val newWine = wine.copy(name = newPrice)
+    println(newWine)
   }
 
   def getInputRateWine = () => {
-    val inputRateWine = getInput("Which wine do you want to rate ");
-    val wine = wineDB.getWineByName(inputRateWine);
-    // do something....
+    val inputRateWine = getInput("Which wine do you want to rate ")
+    val wine = wineDB.getWineByName(inputRateWine)
+    println(wine)
+    val newRate = getInput("On a scale of 1 to 5, how would you rate this wine ?")
+    val newRateAverage = wine.rateWine(newRate.toDouble)
+    val newWine = wine.copy(rating = Rating(newRateAverage, wine.rating.nb_rating+1))
+    println(newWine)
   }
 
 
