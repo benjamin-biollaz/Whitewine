3759f6d47919475bfea175718d5f65d014cd1c99
benjamin <benjamin.biollaz@students.hevs.ch>
2022-12-17 16:02:22 +0100

    documented wine

--- src/main/scala/classes/Wine.scala
+++ src/main/scala/classes/Wine.scala
@@ -1,11 +1,25 @@
 package classes
 
+/**
+ * Wine class contains all properties of a wine.
+ * It is composed of both native types (int, ...) and custom objects (Country, ...)
+ * @param name The name of the wine
+ * @param year The year of the wine.
+ * @param price The price of the wine.
+ * @param country Country class has a name and extends from area.
+ * @param region Region class has a name and extends from area.
+ * @param winery Winery class has a name and extends from area.
+ * @param rating Rating class contains both the number of ratings and the average of ratings.
+ */
 case class Wine(name: String, year: Int, price: Double, country: Country, region: Region, winery: Winery, rating: Rating) {
   override def toString: String = "Name:" + name + " Year:" + year + " Price:" + price + " " + country + " " + region + " " + winery + " " + rating
 
-  // rateWine calculates the new rating average using 
-  // the number of ratings already attributed
+  /**
+   * Calculates the new rating average using
+   * the number of ratings already attributed
+   */
   val rateWine = (newRate: Double) => {
+    // total number of rating weight is respected
     (this.rating.rating * this.rating.nb_rating + newRate) / (this.rating.nb_rating + 1)
   }
 
