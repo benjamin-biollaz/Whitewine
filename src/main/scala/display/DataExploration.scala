f780f7e54f6f313067c3b70560fe4500cbbea985
benjamin <benjamin.biollaz@students.hevs.ch>
2022-12-18 20:55:54 +0100

    Data exploration documentation.

--- src/main/scala/display/DataExploration.scala
+++ src/main/scala/display/DataExploration.scala
@@ -6,13 +6,8 @@ import manipulation.{InputManager, SafeConversion}
 import java.util.Scanner
 
 /**
- *A class with methods to explore the CSV file
- * @param getAllWinesByYear Get the user's year input and call the method getWineByYear from the WineDB class
- * @param getAllWines call the method getAllWines from the WineDB
- * @param getInputWineByName Get the user's input and call the method getWineByName from the WineDB class
- * @param getInputRegion Get the user's region input and call the method getNbWineByRegion from the WineDB class
- * @param getInputWinery Get the user's winery input and call the method getTotalPriceWineryWines from the WineDB class
- * @param getInputCountry Get the user's country input and call the method getAvgRatingCountry from the WineDB class
+ * A class with methods that prompts the user for inputs
+ * and display the corresponding results.
  */
 
 class DataExploration {
@@ -21,7 +16,7 @@ class DataExploration {
   val converter = new SafeConversion()
 
   /**
-   * Console display for the user
+   * Prompt the the user for which data exploration he wants to perform.
    */
   def getInformationFromCSV = () => {
     val inputChoice = inputManager.getInput("What do you want to do ? \n" +
@@ -45,33 +40,52 @@ class DataExploration {
     }
   }
 
+  /**
+   * Get the user's year input and call the method getWineByYear from the WineDB class
+   * Displays all the wines created in a year.
+   */
   def getAllWinesByYear = () => {
     val input = converter.stringToInt(inputManager.getInput("For which year do you want to display wines ?"))
     val wines = wineDB.getWineByYear(input.toInt);
     if (wines == null) println("No wines for " + input) else for (wine <- wines) println(wine)
   }
 
+  /**
+   * Display all wines in the db
+   */
   def getAllWines = () => {
     val wines = wineDB.getAllWines()
     if (wines == null) println("No wines in the dataset") else for (wine <- wines) println(wine)
   }
 
+  /**
+   * Get a wine name input and display the wine
+   */
   def getInputWineByName = () => {
     val inputName = inputManager.getInput("Enter the name of the wine ?");
     val wine = wineDB.getWineByName(inputName);
     if (wine == null) println("Wine not found") else println(wine);
   }
 
+  /**
+   * Get the user's region input and display the number of wines for it
+   */
   def getInputRegion = () => {
     val inputRegion = inputManager.getInput("For which region do you want the total price of wines?");
     println("There's " + wineDB.getNbWineByRegion(inputRegion) + " wines in " + inputRegion);
   }
 
+  /**
+   * Get the user's winery input and display the total price of all wines in it
+   */
   def getInputWinery = () => {
     val inputWinery = inputManager.getInput("For which winery do you want the total price of wines?");
     println("The total price of all the wines from" + inputWinery + " is :" + wineDB.getTotalPriceWineryWines(inputWinery) + "CHF")
   }
 
+  /**
+   * Get the user's country input and display the average rating for the wines from it.
+   */
   def getInputCountry = () => {
     val inputCountry = inputManager.getInput("For which country you want the average rating?")
     val averageRating = wineDB.getAvgRatingCountry(inputCountry)
