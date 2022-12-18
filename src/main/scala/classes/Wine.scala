db4ed45595f3c076b77b67805c9d4607fb92ea94
benjamin <benjamin.biollaz@students.hevs.ch>
2022-12-18 20:36:49 +0100

    rate and discount wine

--- src/main/scala/classes/Wine.scala
+++ src/main/scala/classes/Wine.scala
@@ -3,6 +3,7 @@ package classes
 /**
  * Wine class contains all properties of a wine.
  * It is composed of both native types (int, ...) and custom objects (Country, ...)
+ *
  * @param name The name of the wine
  * @param year The year of the wine.
  * @param price The price of the wine.
@@ -13,14 +14,4 @@ package classes
  */
 case class Wine(name: String, year: Int, price: Double, country: Country, region: Region, winery: Winery, rating: Rating) {
   override def toString: String = "Name:" + name + " Year:" + year + " Price:" + price + " " + country + " " + region + " " + winery + " " + rating
-
-  /**
-   * Calculates the new rating average using
-   * the number of ratings already attributed
-   */
-  val rateWine = (newRate: Double) => {
-    // total number of rating weight is respected
-    (this.rating.rating * this.rating.nb_rating + newRate) / (this.rating.nb_rating + 1)
-  }
-
 }
